64138e21dc73101d8f601bd02693460bd296ded7
RK289g <rksaju80@gmail.com>
2024-01-08 02:20:50 +0600

    contact us finished

--- src/pages/conactUs/ContactUs.jsx
+++ src/pages/conactUs/ContactUs.jsx
@@ -1,6 +1,7 @@
 import {
   ArrowRightOutlined,
   FacebookOutlined,
+  LinkedinOutlined,
   MailOutlined,
   SendOutlined,
 } from "@ant-design/icons";
@@ -29,40 +30,44 @@ const ContactUs = () => {
       </div>
 
       <div className="contact-container">
-        <div className="contact-info">
+        <div>
           <h3 className="contact-title">Talk to us</h3>
-          <div className="contact-card">
-            <MailOutlined className="contact-card-icon" />
-            <h3 className="contact-card-title">Email</h3>
-            <p className="contact-card-data">rksaju80@gmail.com</p>
-            <a href="mailto:rksaju809@gmail.com.com" className="contact-button">
-              Write me <ArrowRightOutlined />
-            </a>
-          </div>
 
           <div className="contact-info">
-            <div className="contact-card">
-              <FacebookOutlined className="contact-card-icon" />
-              <h3 className="contact-card-title">Facebook</h3>
-              <a
-                href="https://www.facebook.com/profile.php?id=100086738076898"
-                className="contact-button"
-              >
-                Connect me <ArrowRightOutlined />
-              </a>
+            <div className="contact-info">
+              <div className="contact-card">
+                <MailOutlined className="contact-card-icon" />
+                <h3 className="contact-card-title">Email</h3>
+                <a href="mailto:rksaju80@gmail.com" className="contact-button">
+                  Connect us <ArrowRightOutlined />
+                </a>
+              </div>
             </div>
-          </div>
 
-          <div className="contact-info">
-            <div className="contact-card">
-              <FacebookOutlined className="contact-card-icon" />
-              <h3 className="contact-card-title">Facebook</h3>
-              <a
-                href="https://www.facebook.com/profile.php?id=100086738076898"
-                className="contact-button"
-              >
-                Connect me <ArrowRightOutlined />
-              </a>
+            <div className="contact-info">
+              <div className="contact-card">
+                <FacebookOutlined className="contact-card-icon" />
+                <h3 className="contact-card-title">Facebook</h3>
+                <a
+                  href="https://www.facebook.com/profile.php?id=100086738076898"
+                  className="contact-button"
+                >
+                  Connect us <ArrowRightOutlined />
+                </a>
+              </div>
+            </div>
+
+            <div className="contact-info">
+              <div className="contact-card">
+                <LinkedinOutlined className="contact-card-icon" />
+                <h3 className="contact-card-title">LinkedIn</h3>
+                <a
+                  href="https://www.facebook.com/profile.php?id=100086738076898"
+                  className="contact-button"
+                >
+                  Connect us <ArrowRightOutlined />
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -91,18 +96,18 @@ const ContactUs = () => {
               />
             </div>
 
-            <div className="contact-form-div contact-form-area">
+            <div className="contact-form-area">
               <label className="contact-form-tag">Project</label>
               <textarea
                 name="project"
                 cols="30"
                 rows="10"
                 className="contact-form-input"
-                placeholder="Write your project"
+                placeholder="Write your message"
               ></textarea>
             </div>
             <button className="button button--flex">
-              Send Message <SendOutlined />
+              Send Message <SendOutlined className="send-button"/>
               <path
                 d="M14.2199 21.9352C13.0399 21.9352 11.3699 21.1052 10.0499 17.1352L9.32988 14.9752L7.16988 14.2552C3.20988 12.9352 2.37988 11.2652 2.37988 10.0852C2.37988 8.91525 3.20988 7.23525 7.16988 5.90525L15.6599 3.07525C17.7799 2.36525 19.5499 2.57525 20.6399 3.65525C21.7299 4.73525 21.9399 6.51525 21.2299 8.63525L18.3999 17.1252C17.0699 21.1052 15.3999 21.9352 14.2199 21.9352ZM7.63988 7.33525C4.85988 8.26525 3.86988 9.36525 3.86988 10.0852C3.86988 10.8052 4.85988 11.9052 7.63988 12.8252L10.1599 13.6652C10.3799 13.7352 10.5599 13.9152 10.6299 14.1352L11.4699 16.6552C12.3899 19.4352 13.4999 20.4252 14.2199 20.4252C14.9399 20.4252 16.0399 19.4352 16.9699 16.6552L19.7999 8.16525C20.3099 6.62525 20.2199 5.36525 19.5699 4.71525C18.9199 4.06525 17.6599 3.98525 16.1299 4.49525L7.63988 7.33525Z"
                 fill="var(--container-color)"
