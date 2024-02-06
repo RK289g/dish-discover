27d67e7c00beda9f8ef1119b97ecc37738940b3a
RK289g <rksaju80@gmail.com>
2024-02-06 22:49:26 +0600

    recipe by categories done

--- src/assets/recipeInHomePage/recipeByCategory/RecipeByCategory.jsx
+++ src/assets/recipeInHomePage/recipeByCategory/RecipeByCategory.jsx
@@ -1,30 +1,95 @@
 import { useNavigate } from "react-router-dom";
 import "./recipeByCategory.css";
+import breakfastItems from "../../../assets/top-categories/breakfast-items.png";
+import dessertItems from "../../../assets/top-categories/dessert-items.png";
+import beginnerItems from "../../../assets/top-categories/starter-items.png";
+import additionalItems from "../../../assets/top-categories/additiona-items.png";
+import { Image } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 const RecipeByCategory = () => {
   const navigate = useNavigate();
   const goToRecipe = (stateValue, CategoryRecipeIndex) => {
-    navigate("/recipes", { state: { CategoryRecipeKey: stateValue, CategoryRecipeIndex } });
+    navigate("/recipes", {
+      state: { CategoryRecipeKey: stateValue, CategoryRecipeIndex },
+    });
   };
 
   return (
     <div className="recipe-category-wrapper">
       <div className="recipe-category-inner-wrapper">
-        <h1 className="title-recipe-category">Recipe By Category</h1>
+        <h1 className="title-recipe-category">Top Categories</h1>
 
         <div className="column-recipe-category">
           <div
             className="category-card-container"
             onClick={() => goToRecipe("Breakfast", 1)}
           >
-            <img
-              className="img-recipe-category"
-              src="https://www.themealdb.com/images/media/meals/hqaejl1695738653.jpg"
-              alt=""
-            />
-            <h1 className="text-recipe-category">Breakfast</h1>
+            <div className="upper-card-wrapper">
+              <h1 className="text-recipe-category">Breakfast Items</h1>
+              <Image
+                src={breakfastItems}
+                preview={false}
+                className="card-image"
+              />
+            </div>
+            <div className="lower-card-wrapper">
+              <h4>300+ recipes</h4>
+              <ArrowRightOutlined />
+            </div>
           </div>
           <div
+            className="category-card-container"
+            onClick={() => goToRecipe("Dessert", 3)}
+          >
+            <div className="upper-card-wrapper">
+              <h1 className="text-recipe-category">Dessert Items</h1>
+              <Image
+                src={dessertItems}
+                preview={false}
+                className="card-image"
+              />
+            </div>
+            <div className="lower-card-wrapper">
+              <h4>300+ recipes</h4>
+              <ArrowRightOutlined />
+            </div>
+          </div>
+          <div
+            className="category-card-container"
+            onClick={() => goToRecipe("Starter", 11)}
+          >
+            <div className="upper-card-wrapper">
+              <h1 className="text-recipe-category">Starter Items</h1>
+              <Image
+                src={beginnerItems}
+                preview={false}
+                className="card-image"
+              />
+            </div>
+            <div className="lower-card-wrapper">
+              <h4>300+ recipes</h4>
+              <ArrowRightOutlined />
+            </div>
+          </div>
+          <div
+            className="category-card-container"
+            onClick={() => goToRecipe("Side", 10)}
+          >
+            <div className="upper-card-wrapper">
+              <h1 className="text-recipe-category">Side Items</h1>
+              <Image
+                src={additionalItems}
+                preview={false}
+                className="card-image"
+              />
+            </div>
+            <div className="lower-card-wrapper">
+              <h4>300+ recipes</h4>
+              <ArrowRightOutlined />
+            </div>
+          </div>
+          {/* <div
             className="category-card-container"
             onClick={() => goToRecipe("Starter", 11)}
           >
@@ -57,7 +122,7 @@ const RecipeByCategory = () => {
               alt=""
             />
             <h1 className="text-recipe-category">Side</h1>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
