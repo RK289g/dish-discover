c55723635b4ba886e83531ce5c3da37b786a5610
RK289g <rksaju80@gmail.com>
2024-02-08 03:27:01 +0600

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,22 +4,42 @@ import { useState } from "react";
 import LinkedinIcon from "../common/logo/LinkedInIcon";
 import TwitterIcon from "../common/logo/TwitterIcon";
 import FacebookIcon from "../common/logo/FacebookIcon";
+import { Divider } from "antd";
 
 const Footer = () => {
   const [currentYear] = useState(new Date().getFullYear());
   return (
     <footer className="footer-wrapper ">
+      <div className="quick-links">
+        <div className="quick-link-wrapper">
+          <Link to="/" className="items font-poppins">
+            Home
+          </Link>
+          <Divider type="vertical" className="footer-divider" />
+          <Link to="/recipes" className="items">
+            Recipes
+          </Link>
+          <Divider type="vertical" className="footer-divider" />
+          <Link to="/random-recipe" className="items">
+            Random Recipe
+          </Link>
+          <Divider type="vertical" className="footer-divider" />
+          <Link to="/contact-us" className="items">
+            Contact Us
+          </Link>
+        </div>
+      </div>
       <h4 className="footer-heading font-inter">Connect with us</h4>
       <div className="contact-section-wrapper">
         <div className="contact-section">
           <div>
             <Link
-              to="https://www.linkedin.com/in/raisul-karim-saju/"
+              to="https://www.facebook.com/profile.php?id=100086738076898"
               target="_blank"
               rel="noopener noreferrer"
               className="footer-user-link"
             >
-              <LinkedinIcon />
+              <FacebookIcon />
             </Link>
           </div>
           <div>
@@ -32,37 +52,21 @@ const Footer = () => {
               <TwitterIcon />
             </Link>
           </div>
-
           <div>
             <Link
-              to="https://www.facebook.com/profile.php?id=100086738076898"
+              to="https://www.linkedin.com/in/raisul-karim-saju/"
               target="_blank"
               rel="noopener noreferrer"
               className="footer-user-link"
             >
-              <FacebookIcon />
+              <LinkedinIcon />
             </Link>
           </div>
         </div>
       </div>
 
-      <div className="quick-links">
-        {/* <h4>Quick Links</h4> */}
-        <div className="quick-link-wrapper">
-          <Link to="/recipes" className="items">
-            Recipes
-          </Link>
-          <Link to="/random-recipe" className="items">
-            Random Recipe
-          </Link>
-          {/* <Link className="items">Articles</Link> */}
-          <Link to="/contact-us" className="items">
-            Contact Us
-          </Link>
-        </div>
-      </div>
       <div>
-        <p className="copyright-text">
+        <p className="copyright-text font-inter">
           © {currentYear} Dish Discover. All rights reserved.
         </p>
       </div>
