113e956ca97a52e73bda6349137a7f2757ff27b3
RK289g <rksaju80@gmail.com>
2024-02-21 03:56:23 +0600

    instruction section added

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,4 +1,4 @@
-import { Divider, Image } from "antd";
+import { Collapse, Divider, Image } from "antd";
 import "./Recipe.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -11,6 +11,7 @@ import {
   ShareAltOutlined,
   StarFilled,
 } from "@ant-design/icons";
+import CollapsePanel from "antd/es/collapse/CollapsePanel";
 
 const Recipe = () => {
   const { recipeId } = useParams();
@@ -107,70 +108,23 @@ const Recipe = () => {
                     </div>
                   </div>
                 </div>
-                {/* <div className="top-text">
-                  <h1>{recData?.strMeal}</h1>
-                  <h3>
-                    Cuisine: <span>{recData?.strArea}</span>
-                  </h3>
-                  {recData?.strTags && (
-                    <h3>
-                      Features: <span>{recData?.strTags}</span>
-                    </h3>
-                  )}
-
-                  <h3>
-                    Course: <span>{recData?.strCategory}</span>
-                  </h3>
-                </div> */}
-              </div>
-              <div className="table-div">
-                <h2>Ingredients</h2>
-
-                <table>
-                  {[...Array(20)].map((_, index) => {
-                    const ingredientKey = `strIngredient${index + 1}`;
-                    const measureKey = `strMeasure${index + 1}`;
-                    const ingredientImageSrc = `https://www.themealdb.com/images/ingredients/${recData[ingredientKey]}-small.png`;
-
-                    if (recData[ingredientKey]) {
-                      return (
-                        <tr key={index}>
-                          <td>
-                            <Image
-                              src={ingredientImageSrc}
-                              preview={false}
-                              height={50}
-                            />
-                          </td>
-                          <td>{recData[ingredientKey]}</td>
-                          <td>{recData[measureKey]}</td>
-                        </tr>
-                      );
-                    }
-
-                    return null;
-                  })}
-                </table>
               </div>
               <div className="instruction-div">
-                <h3>Instructions</h3>
-                <ul>
+                <h1>instructions</h1>
+
+                <Collapse>
                   {recData?.strInstructions
-                    .split("\r\n\r\n")
+                    .split(/\r?\n/)
+                    .filter((instruction) => instruction.trim() !== "") // Remove empty instructions
                     .map((instruction, index) => (
-                      <div
-                        key={index}
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "flex-start",
-                        }}
+                      <CollapsePanel
+                        header={`STEP ${index + 1}`}
+                        key={index + 1}
                       >
-                        <p>Step {index + 1}</p>
-                        <li>{instruction}</li>
-                      </div>
+                        <p className="instruction-text font-inter">{instruction}</p>
+                      </CollapsePanel>
                     ))}
-                </ul>
+                </Collapse>
               </div>
 
               <div className="YT-wrapper">
