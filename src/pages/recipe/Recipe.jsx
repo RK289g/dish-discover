bbc9a64b8c6a08a9fc9b3e98c5309ef068bc97ac
RK289g <rksaju80@gmail.com>
2024-02-22 16:25:50 +0600

    detail page modified

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,4 +1,4 @@
-import { Collapse, Divider, Image } from "antd";
+import { Col, Collapse, Divider, Image, Row } from "antd";
 import "./Recipe.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -44,90 +44,85 @@ const Recipe = () => {
       {recipeIdData?.map((recData) => {
         return (
           <div className="wrapper" key={recData?.idMeal}>
-            <div className="inner-wrapper">
-              <div className="recipe-top">
-                <div className="image-wrapper">
-                  <Image
-                    preview={false}
-                    className="image"
-                    src={recData?.strMealThumb}
-                    alt="ThumbNail"
-                  />
+            <Row className="inner-wrapper">
+              <Col xs={24} md={12} className="image-wrapper">
+                <Image
+                  preview={false}
+                  className="image-class"
+                  src={recData?.strMealThumb}
+                  alt="ThumbNail"
+                />
+              </Col>
+              <Col xs={24} md={12} className="text-wrapper">
+                <div className="recipe-text-inner-wrapper">
+                  <h1 className="recipe-title font-inter">
+                    {recData?.strMeal}
+                  </h1>
+                  <p className="recipe-text font-inter">
+                    Cuisine: {recData?.strArea}
+                  </p>
+                  {recData?.strTags && (
+                    <p className="recipe-text">Features: {recData?.strTags}</p>
+                  )}
                 </div>
-                <div>
-                  <div className="recipe-text-inner-wrapper">
-                    <h1 className="recipe-title font-inter">
-                      {recData?.strMeal}
-                    </h1>
-                    <p className="recipe-text font-inter">
-                      Cuisine: {recData?.strArea}
-                    </p>
-                    {recData?.strTags && (
-                      <p className="recipe-text">
-                        Features: {recData?.strTags}
-                      </p>
-                    )}
+                <div className="icons">
+                  <div className="icons-wrapper-left">
+                    <HeartOutlined /> <h5>12 Likes</h5>
                   </div>
-                  <div>
-                    <div className="icons">
-                      <div className="icons-wrapper-left">
-                        <HeartOutlined />
-                      </div>
-                      <div className="icons-wrapper-left">
-                        <CommentOutlined />
-                      </div>
-                      <div className="icons-wrapper-right">
-                        <ShareAltOutlined />
-                      </div>
-                      <div className="icons-wrapper-right">
-                        <SaveOutlined />
-                      </div>
-                    </div>
+                  <div className="icons-wrapper-left">
+                    <CommentOutlined /> <h5>48 Comments</h5>
                   </div>
-                  <div>
-                    <div className="rating-wrapper">
-                      <h3 className="recipe-text font-inter">
-                        Average Ratings
-                      </h3>
-                      <StarFilled />
-                      <StarFilled />
-                      <StarFilled />
-                      <StarFilled />
-                      <StarFilled />
-                    </div>
-                    <div className="rating-wrapper">
-                      <div>
-                        <h1 className="recipe-title font-inter">20</h1>
-                        <p className="recipe-text font inter">Ingredients</p>
-                      </div>
-                      <Divider type="vertical" className="rating-divider" />
-                      <div>
-                        <h1 className="recipe-title font-inter">10</h1>
-                        <p className="recipe-text font-inter">Minutes</p>
-                      </div>
-                    </div>
+                  <div className="icons-wrapper-right">
+                    <ShareAltOutlined /> <h5>Share</h5>
+                  </div>
+                  <div className="icons-wrapper-right">
+                    <SaveOutlined /> <h5>Save</h5>
                   </div>
                 </div>
-              </div>
-              <div className="instruction-div">
-                <h1>instructions</h1>
+                <div className="rating-wrapper">
+                  <h3 className="recipe-text font-inter">Average Ratings</h3>
+                  <StarFilled />
+                  <StarFilled />
+                  <StarFilled />
+                  <StarFilled />
+                  <StarFilled />
+                </div>
+                <div className="rating-wrapper">
+                  <div>
+                    <h1 className="recipe-title font-inter">20</h1>
+                    <p className="recipe-text font inter">Ingredients</p>
+                  </div>
+                  <Divider type="vertical" className="rating-divider" />
+                  <div>
+                    <h1 className="recipe-title font-inter">10</h1>
+                    <p className="recipe-text font-inter">Minutes</p>
+                  </div>
+                </div>
+              </Col>
 
+              <Col xs={24} md={12}>
+                <h1>Ingredients</h1>
+              </Col>
+              <Col xs={24} md={12} className="instruction-div">
+                <h1>Instructions</h1>
                 <Collapse>
                   {recData?.strInstructions
                     .split(/\r?\n/)
-                    .filter((instruction) => instruction.trim() !== "") // Remove empty instructions
+                    .filter((instruction) => instruction.trim() !== "")
                     .map((instruction, index) => (
                       <CollapsePanel
                         header={`STEP ${index + 1}`}
                         key={index + 1}
                       >
-                        <p className="instruction-text font-inter">{instruction}</p>
+                        <p className="instruction-text font-inter">
+                          {instruction}
+                        </p>
                       </CollapsePanel>
                     ))}
                 </Collapse>
-              </div>
+              </Col>
 
-              <div className="YT-wrapper">
+              <Col span={24} className="YT-wrapper">
                 <h3>Watch Video</h3>
                 <iframe
                   className="i-frame"
@@ -140,8 +135,8 @@ const Recipe = () => {
                   allowFullScreen
                 ></iframe>
                 <h4>Video on {recData?.strMeal}</h4>
-              </div>
-            </div>
+              </Col>
+            </Row>
           </div>
         );
       })}
