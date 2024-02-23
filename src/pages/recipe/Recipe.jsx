1b9c265e61c5304a8a98c69b3887eebaaacc6f49
RK289g <rksaju80@gmail.com>
2024-02-23 16:35:42 +0600

    detail page modified

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -10,6 +10,7 @@ import {
   SaveOutlined,
   ShareAltOutlined,
   StarFilled,
+  StarOutlined,
 } from "@ant-design/icons";
 import CollapsePanel from "antd/es/collapse/CollapsePanel";
 
@@ -44,16 +45,18 @@ const Recipe = () => {
       {recipeIdData?.map((recData) => {
         return (
           <div className="wrapper" key={recData?.idMeal}>
-            <Row className="inner-wrapper">
-              <Col xs={24} md={12} className="image-wrapper">
-                <Image
-                  preview={false}
-                  className="image-class"
-                  src={recData?.strMealThumb}
-                  alt="ThumbNail"
-                />
+            <Row gutter={[80, 80]} className="inner-wrapper">
+              <Col xs={24} md={12} lg={12} align="middle">
+                <div className="image-wrapper">
+                  <Image
+                    preview={false}
+                    className="image-class"
+                    src={recData?.strMealThumb}
+                    alt="ThumbNail"
+                  />
+                </div>
               </Col>
-              <Col xs={24} md={12} className="text-wrapper">
+              <Col xs={24} md={12}>
                 <div className="recipe-text-inner-wrapper">
                   <h1 className="recipe-title font-inter">
                     {recData?.strMeal}
@@ -64,42 +67,44 @@ const Recipe = () => {
                   {recData?.strTags && (
                     <p className="recipe-text">Features: {recData?.strTags}</p>
                   )}
-                </div>
-                <div className="icons">
-                  <div className="icons-wrapper-left">
-                    <HeartOutlined /> <h5>12 Likes</h5>
+                  <div className="icons font-inter">
+                    <div className="icons-wrapper-left">
+                      <HeartOutlined /> <h5>12 Likes</h5>
+                    </div>
+                    <div className="icons-wrapper-left">
+                      <CommentOutlined /> <h5>48 Comments</h5>
+                    </div>
+                    <div className="icons-wrapper-right">
+                      <ShareAltOutlined /> <h5>Share</h5>
+                    </div>
+                    <div className="icons-wrapper-right">
+                      <SaveOutlined /> <h5>Save</h5>
+                    </div>
                   </div>
-                  <div className="icons-wrapper-left">
-                    <CommentOutlined /> <h5>48 Comments</h5>
+                  <div className="rating-wrapper">
+                    <h3 className="rating-icons font-inter">Average Ratings</h3>
+                    <StarFilled className="rating-icon-color"/>
+                    <StarFilled className="rating-icon-color"/>
+                    <StarFilled className="rating-icon-color"/>
+                    <StarFilled className="rating-icon-color"/>
+                    <StarOutlined className="rating-icon-color"/>
                   </div>
-                  <div className="icons-wrapper-right">
-                    <ShareAltOutlined /> <h5>Share</h5>
-                  </div>
-                  <div className="icons-wrapper-right">
-                    <SaveOutlined /> <h5>Save</h5>
-                  </div>
-                </div>
-                <div className="rating-wrapper">
-                  <h3 className="recipe-text font-inter">Average Ratings</h3>
-                  <StarFilled />
-                  <StarFilled />
-                  <StarFilled />
-                  <StarFilled />
-                  <StarFilled />
-                </div>
-                <div className="rating-wrapper">
-                  <div>
-                    <h1 className="recipe-title font-inter">20</h1>
-                    <p className="recipe-text font inter">Ingredients</p>
-                  </div>
-                  <Divider type="vertical" className="rating-divider" />
-                  <div>
-                    <h1 className="recipe-title font-inter">10</h1>
-                    <p className="recipe-text font-inter">Minutes</p>
+                  <div className="rating-wrapper">
+                    <div className="rating-inner-wrapper">
+                      <h1 className="recipe-title font-inter">20</h1>
+                      <p className="rating-text font-inter">Ingredients</p>
+                    </div>
+                    <Divider type="vertical" className="rating-divider" />
+                    <div className="rating-inner-wrapper">
+                      <h1 className="recipe-title font-inter">10</h1>
+                      <p className="rating-text font-inter">Minutes</p>
+                    </div>
                   </div>
                 </div>
               </Col>
+            </Row>
 
+            <Row>
               <Col xs={24} md={12}>
                 <h1>Ingredients</h1>
               </Col>
@@ -121,22 +126,22 @@ const Recipe = () => {
                     ))}
                 </Collapse>
               </Col>
-
-              <Col span={24} className="YT-wrapper">
-                <h3>Watch Video</h3>
-                <iframe
-                  className="i-frame"
-                  src={`https://www.youtube.com/embed/${getYoutubeVideoId(
-                    recData?.strYoutube
-                  )}`}
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowFullScreen
-                ></iframe>
-                <h4>Video on {recData?.strMeal}</h4>
-              </Col>
             </Row>
+
+            <div span={24} className="YT-wrapper">
+              <h3>Watch Video</h3>
+              <iframe
+                className="i-frame"
+                src={`https://www.youtube.com/embed/${getYoutubeVideoId(
+                  recData?.strYoutube
+                )}`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+              <h4>Video on {recData?.strMeal}</h4>
+            </div>
           </div>
         );
       })}
