fa51176f2459dc7c2ab99aa9fc7251683305d071
RK289g <rksaju80@gmail.com>
2024-03-15 01:10:12 +0600

    recipe card modified in recipes page

--- src/components/common/recipe-card/RecipeCard.jsx
+++ src/components/common/recipe-card/RecipeCard.jsx
@@ -1,12 +1,12 @@
 import "./RecipeCard.css";
 import PropTypes from "prop-types";
-import { Button, Card, Col, Row} from "antd";
+import { Button, Card, Col, Row, Skeleton } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { RightOutlined } from "@ant-design/icons";
 
 const RecipeCard = ({ recipeData }) => {
-  const [visibleRecipes, setVisibleRecipes] = useState(6);
+  const [visibleRecipes, setVisibleRecipes] = useState(9);
   const navigate = useNavigate();
 
   const handleRecipeClick = (recipeId) => {
@@ -18,54 +18,67 @@ const RecipeCard = ({ recipeData }) => {
   };
 
   return (
-    <div className="cuisines-type-wrapper">
-      <Row gutter={50} style={{ margin: "0px", padding: "0px 20px" }}>
-        {recipeData?.slice(0, visibleRecipes).map((recData) => {
-          return (
-            <Col xl={8} md={12} sm={24} key={recData?.idMeal}>
-              <Card hoverable className="card">
-                  <div>
-                    <img
-                      className="image"
-                      src={recData?.strMealThumb}
-                      alt="ThumbNail"
-                    />
-                    <div className="card-text">
-                      <h1 className="font-inter">
-                        {recData?.strMeal && recData.strMeal.length > 18
-                          ? recData.strMeal.substring(0, 20) + " ..."
-                          : recData.strMeal}
-                      </h1>
-                      <div className="text-btn-wrapper">
-                        <h5 className="font-inter">
-                          {recData?.strInstructions.substring(0, 90) + " ..."}
-                        </h5>
-                      </div>
-                      <div className="button-wrapper">
-                        <Button
-                          onClick={() => {
-                            handleRecipeClick(recData.idMeal);
-                          }}
-                          className="card-button"
-                          icon={<RightOutlined />}
-                        >
-                          See Recipe
-                        </Button>
+    <div className="home-wrapper">
+      <div className="cuisines-type-wrapper card-wrapper">
+        <Row
+          align={"middle"}
+          gutter={[16, 16]}
+          style={{ margin: "0px", padding: "0px 0px" }}
+        >
+          {recipeData?.slice(0, visibleRecipes).map((recData) => {
+            return (
+              <Col xl={8} md={12} sm={24} key={recData?.idMeal}>
+                <Card hoverable className="card">
+                  <Skeleton
+                    active={true}
+                    loading={false}
+                    avatar={{ shape: "square", size: 350 }}
+                  >
+                    <div>
+                      <img
+                        className="image"
+                        src={recData?.strMealThumb}
+                        alt="ThumbNail"
+                      />
+                      <div className="card-text">
+                        <h1 className="font-inter">
+                          {recData?.strMeal && recData.strMeal.length > 18
+                            ? recData.strMeal.substring(0, 20) + " ..."
+                            : recData.strMeal}
+                        </h1>
+                        <div className="text-btn-wrapper">
+                          <h5 className="font-inter">
+                            Enjoy this delicious recipe! Easy to make and
+                            bursting with flavor. Perfect for any occasion.
+                          </h5>
+                        </div>
+                        <div className="button-wrapper">
+                          <Button
+                            onClick={() => {
+                              handleRecipeClick(recData.idMeal);
+                            }}
+                            className="card-button"
+                            icon={<RightOutlined />}
+                          >
+                            See Recipe
+                          </Button>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </Skeleton>
                 </Card>
-            </Col>
-          );
-        })}
-      </Row>
-      {visibleRecipes < recipeData?.length && (
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-          <Button className="btn-see-more" onClick={handleSeeMore}>
-            See More
-          </Button>
-        </div>
-      )}
+              </Col>
+            );
+          })}
+        </Row>
+        {visibleRecipes < recipeData?.length && (
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <Button className="btn-see-more" onClick={handleSeeMore}>
+              See More
+            </Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
