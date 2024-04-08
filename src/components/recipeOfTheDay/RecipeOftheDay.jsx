836c95dee968aaf5d8b22a6d63e066d2835fbcf7
RK289g <rksaju80@gmail.com>
2024-04-09 03:44:59 +0600

    home page fixed

--- src/components/recipeOfTheDay/RecipeOftheDay.jsx
+++ src/components/recipeOfTheDay/RecipeOftheDay.jsx
@@ -13,36 +13,30 @@ const RecipeOftheDay = () => {
 
   return (
     <div className="recipe-category-wrapper">
-      <div className="recipe-category=inner-wrapper">
-        <h1 className="title-recipe-category font-fanlste">
-          Recipe Of the Day
-        </h1>
-        <div className="day-card-wrapper">
-          <div className="day-card-inner-wrapper">
-            <div className="day-image-wrapper">
-              <Image src={dayImage} preview={false} className="day-image" />
-            </div>
-            <div className="day-text">
-              <div className="day-text-category card-text-category">
-                <p className="font-inter">French</p>
-              </div>
-              <h2 className="day-text-header font-inter">Chicken Parmentier</h2>
-              <h5 className="day-text-paragraph font-inter">
-                French favorite featuring breaded and fried chicken
-                <br /> cutlets topped with marinara sauce and melted <br />{" "}
-                cheese, served with pasta.
-              </h5>
-              <Button
-                className="day-recipe-button"
-                icon={<RightOutlined className="Right-Outlined" />}
-                onClick={() => {
-                  recipeOftheDayClick(52879);
-                }}
-              >
-                See Recipe
-              </Button>
-            </div>
+      <h1 className="title-recipe-category font-fanlste">Recipe Of the Day</h1>
+      <div className="day-card-wrapper">
+        <div className="day-image-wrapper">
+          <Image src={dayImage} preview={false} className="day-image" />
+        </div>
+        <div className="day-text">
+          <div className="day-text-category card-text-category">
+            <p className="font-inter">French</p>
           </div>
+          <p className="day-text-header font-inter">Chicken Parmentier</p>
+          <h5 className="day-text-paragraph font-inter">
+            French favorite featuring breaded and fried chicken
+            <br /> cutlets topped with marinara sauce and melted <br /> cheese,
+            served with pasta.
+          </h5>
+          <Button
+            className="day-recipe-button font-inter"
+            icon={<RightOutlined className="Right-Outlined" />}
+            onClick={() => {
+              recipeOftheDayClick(52879);
+            }}
+          >
+            See Recipe
+          </Button>
         </div>
       </div>
     </div>
