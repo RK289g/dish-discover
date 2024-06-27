f35abf17aad243895441ea468db35659a80e5ed7
RK289g <rksaju80@gmail.com>
2024-06-27 15:55:24 +0600

    animation added in hero text

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import heroImage1 from "../../assets/hero-images/hero-image-1.png";
 import heroImage2 from "../../assets/hero-images/hero-image-5.png";
 import arrow from "../../assets/hero-images/arrow.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const navigate = useNavigate();
@@ -17,30 +18,55 @@ const Hero = () => {
       <div className="hero-image-1-wrapper">
         <Image src={heroImage1} preview={false} className="hero-image-1" />
       </div>
-      <div className="hero-text-wrapper">
-        <h1 className="hero-title font-fanlste">
-          Discover more than <br /> <span>5000+</span> unique <br /> Recipes
-        </h1>
-        <h3 className="hero-text">
+      <motion.div
+        className="hero-text-wrapper"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <motion.h1
+          className="hero-title font-fanlste"
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          Discover more than
+        </motion.h1>
+        <motion.h1
+          className="hero-title font-fanlste"
+          initial={{ x: 50, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          <span className="hero-title-span">5000+</span> unique <br /> Recipes
+        </motion.h1>
+        <motion.h3
+          className="hero-text"
+          initial={{ x: -50, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
           Search, Save, Share your favourite
           <br />
           recipes instantly
-        </h3>
-        <Button
-          size="large"
-          className="explore-CTA-button"
-          onClick={() => handleExploreRecipe()}
-          icon={
-            <Image
-              src={arrow}
-              preview={false}
-              className="CTA-button-icon"
-            />
-          }
+        </motion.h3>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 1.5 }}
         >
-          Start Explore
-        </Button>
-      </div>
+          <Button
+            size="large"
+            className="explore-CTA-button"
+            onClick={() => handleExploreRecipe()}
+            icon={
+              <Image src={arrow} preview={false} className="CTA-button-icon" />
+            }
+          >
+            Start Explore
+          </Button>
+        </motion.div>
+      </motion.div>
       <div className="hero-image-2-wrapper">
         <Image src={heroImage2} preview={false} className="hero-image-2" />
       </div>
